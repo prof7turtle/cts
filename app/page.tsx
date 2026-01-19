7ede9b95d4ef46640c491e24fbd8b93635fe6e65
atharv <ad8176178@gmail.com>
2026-01-19 19:08:06 +0530

    Initialized and configured tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>Welcome to Next.js 16!</h1>
+      <h1 className="text-3xl">Welcome to Next.js 16!</h1>
       <p>Get started by editing <code>app/page.tsx</code></p>
     </main>
   )
